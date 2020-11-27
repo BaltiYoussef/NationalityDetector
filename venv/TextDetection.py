fba86c8638f33980c1555e467e9a88da7e00b061
youssef.balti <youssefbalti96@gmail.com>
2020-11-27 17:27:07 +0100

    Initial commit

---
+++ venv/TextDetection.py
@@ -0,0 +1,10 @@
+import cv2
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+img = cv2.imread("test.png")
+#pytesseract only accept RGB values, and open cv read it BGR, so we have to convert it.
+#img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+print(pytesseract.image_to_string(img))
+cv2.imshow('Result',img)
+cv2.waitKey(0)
+img = cv
